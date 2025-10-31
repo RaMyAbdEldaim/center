a10674bcf4c0927ec1d88cdb787977b05e0391b7
RaMyAbdEldaim <r88182050@gmail.com>
2025-10-31 16:15:16 +0200

    first commit

---
+++ my-app/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from './Hero.jsx'
+import FeaturesSection from './FeaturesSection.jsx'
+function App() {
+  return (
+    <>
+        <Hero/>
+        <FeaturesSection/>
+    </>
+      )
+}
+
+export default App
