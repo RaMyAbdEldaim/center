f6fbace14a1655ddaaaa67ed8bbb62865e1a146e
RaMyAbdEldaim <r88182050@gmail.com>
2025-11-01 21:56:03 +0200

    first commit

--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from './Hero.jsx'
 import FeaturesSection from './FeaturesSection.jsx'
+import Courses from './Courses.jsx'
 function App() {
   return (
     <>
         <Hero/>
         <FeaturesSection/>
+        <Courses/>
     </>
       )
 }
